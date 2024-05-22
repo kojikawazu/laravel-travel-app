a963c4a4fe5e73ca77238c6b0731ea64bfcf1ecc
koji kawazu <nanairo7.daydream21@gmail.com>
2024-05-22 18:35:22 +0900

    vercel対応

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Update and install PHP and Composer
-apt-get update && apt-get install -y php-cli php-mbstring unzip curl
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
 # Install dependencies
 composer install
 npm install
